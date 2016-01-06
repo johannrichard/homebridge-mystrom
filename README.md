958506eaa0fc902868a346b57ecd5b44685923c4
Johann Richard <johann.richard@gmail.com>
2016-01-06 15:27:51 +0000

    Initial version of a myStrom Plugin for homebridge

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # homebridge-http-advanced
 
-Supports https devices on the HomeBridge Platform and provides a real time polling for getting the "On" and brightness level characteristics to Homekit. Includes Switch, Light, Door, Smoke and Motion sensor polling.
+Supports myStrom (http://mystrom.ch) devices on the HomeBridge Platform and provides a real time polling for getting the "On" and power level characteristics to Homekit. Implements an "Outlet" service for HomeKit.
 
 # Installation
 
@@ -10,25 +10,14 @@ Supports https devices on the HomeBridge Platform and provides a real time polli
 
 # Configuration
 
-The configuration for this plugin is the same as [homebridge-http](https://github.com/rudders/homebridge-http) but includes an additional method to read the power state of the device and the brightness level. Specify the `status_url` in your config.json that returns the status of the device as an integer (0 = off, 1 = on). Specify the `brightnesslvl_url` to return the current brighness level as an integer.
-
 Configuration sample:
 
  ```
 "accessories": [ 
 	{
-		"accessory": "Http-advanced",
-		"name": "Alfresco Lamp",
-		"switchHandling": "yes",
-				"http_method": "GET",
-				"on_url":      "http://localhost/controller/1700/ON",
-				"off_url":     "http://localhost/controller/1700/OFF",
-				"status_url":  "http://localhost/status/100059",
-		"service": "Light",
-				"brightnessHandling": "yes",
-				"brightness_url":     "http://localhost/controller/1707/%b",
-				"brightnesslvl_url":  "http://localhost/status/100054"			
-									    
+		"accessory": "myStrom",
+		"name": "myStrom WLAN Energy Control Switch",
+		"switch_address": "192.168.1.42",
        } 
     ]
 ```
--- index.js
+++ index.js
@@ -5,42 +5,28 @@ var pollingtoevent = require('polling-to-event');
 module.exports = function(homebridge){
 	Service = homebridge.hap.Service;
 	Characteristic = homebridge.hap.Characteristic;
-	homebridge.registerAccessory("homebridge-http-advanced", "Http-advanced", HttpAdvancedAccessory);
+	homebridge.registerAccessory("homebridge-mystrom", "myStrom", myStromAccessory);
 }
 
 
-function HttpAdvancedAccessory(log, config) {
+function myStromAccessory(log, config) {
   this.log = log;
 
- 	// url info
-	this.on_url                 = config["on_url"];
-	this.on_body                = config["on_body"];
-	this.off_url                = config["off_url"];
-	this.off_body               = config["off_body"];
-	this.lock_url               = config["lock_url"]; 
-	this.lock_body              = config["lock_body"];
-	this.unlock_url             = config["unlock_url"]  			|| this.lock_url;
-	this.unlock_body            = config["unlock_body"] 			|| this.lock_body;
-	this.status_url             = config["status_url"];
-	this.brightness_url         = config["brightness_url"];
-	this.brightnesslvl_url      = config["brightnesslvl_url"];
-	this.http_method            = config["http_method"] 	  	 	|| "GET";
-	this.http_brightness_method = config["http_brightness_method"] 	|| this.http_method;
-	this.http_lock_method       = config["http_lock_method"] 	 	|| this.http_method;
-	this.username               = config["username"] 	  	 	 	|| "";
-	this.password               = config["password"] 	  	 	 	|| "";
-	this.sendimmediately        = config["sendimmediately"] 	 	|| "";
-	this.service                = config["service"] 	  	 	 	|| "Switch";
+ 	// Configuration
 	this.name                   = config["name"];
-	this.brightnessHandling     = config["brightnessHandling"] 	 	|| "no";
-	this.switchHandling 	    = config["switchHandling"] 		 	|| "no";
+
+	this.base_url         		= "http://" + config["switch_address"] ;
+	this.on_url                 = this.base_url + "/relay?state=1";
+	this.off_url                = this.base_url + "/relay?state=0";
+	this.status_url            	= this.base_url + "/report";
 
 	this.state = false;
+	this.inUse = false;
 
-    	var that = this;
+    var that = this;
 
 	var emitter = pollingtoevent(function(done) {
-        	that.httpRequest(that.status_url, "", "GET", that.username, that.password, that.sendimmediately, function(error, response, responseBody) {
+        	that.httpRequest(that.status_url, function(error, response, responseBody) {
             		if (error) {
                 		that.log('HTTP get power function failed: %s', error.message);
 		                callback(error);
@@ -48,67 +34,36 @@ function HttpAdvancedAccessory(log, config) {
 		                done(null, responseBody);
             		}
         	})
-    	}, {longpolling:true,interval:3000});
+    	}, {longpolling:true,interval:2000});
 
-	emitter.on("longpoll", function(data) {       
-        	var binaryState = parseInt(data);
-	        that.state = binaryState > 0;
-		that.log(that.service, "received data from url:"+that.status_url, "state is currently", binaryState); 
+	emitter.on("longpoll", function(data) {   
+        	var relay = JSON.parse(data)['relay'];
+        	var power = JSON.parse(data)['power'];
 
-	switch (that.service) {
-		case "Switch":
-			if (that.switchService ) {
-				that.switchService .getCharacteristic(Characteristic.On)
-				.setValue(that.state);
+	        that.state = relay;
+	        that.inUse = power > 0;
+			that.log(that.service, "received data from url:"+that.status_url, "state is currently", data.toString()); 
+			
+			if (that.outletService ) {
+				that.outletService.getCharacteristic(Characteristic.On)
+				.setValue(that.state)
+				.getCharacteristic(Characteristic.OutletInUse)
+				.setValue(that.inUse);
 			}
-			break;
-		case "Light":
-			if (that.lightbulbService) {
-				that.lightbulbService.getCharacteristic(Characteristic.On)
-				.setValue(that.state);
-			}		
-			break;
-		case "Smoke":
-			if (that.smokeService) {
-				that.smokeService.getCharacteristic(Characteristic.SmokeDetected)
-				.setValue(that.state);
-			}
-			break;
-		case "Motion":
-			if (that.motionService) {
-				that.motionService.getCharacteristic(Characteristic.MotionDetected)
-				.setValue(that.state);
-			}		
-			break;
-		}        
     });
-
-
 }
 
-HttpAdvancedAccessory.prototype = {
+myStromAccessory.prototype = {
 
-	httpRequest: function(url, body, method, username, password, sendimmediately, callback) {
-		request({
-			url: url,
-			body: body,
-			method: method,
-			rejectUnauthorized: false,
-			auth: {
-				user: username,
-				pass: password,
-				sendImmediately: sendimmediately
-			}
-		},
-		function(error, response, body) {
+	httpRequest: function(url, callback) {
+		request.get(url, function(error, response, body) {
 			callback(error, response, body)
 		})
 	},
 
 	setPowerState: function(powerOn, callback) {
 		var url;
-		var body;
-	
+
 		if (!this.on_url || !this.off_url) {
 			this.log.warn("Ignoring request; No power url defined.");
 			callback(new Error("No power url defined."));
@@ -117,15 +72,13 @@ HttpAdvancedAccessory.prototype = {
 	
 		if (powerOn) {
 			url = this.on_url;
-			body = this.on_body;
 			this.log("Setting power state to on");
 		} else {
 			url = this.off_url;
-			body = this.off_body;
 			this.log("Setting power state to off");
 		}
 	
-		this.httpRequest(url, body, this.http_method, this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
+		this.httpRequest(url, function(error, response, responseBody) {
 			if (error) {
 				this.log('HTTP set power function failed: %s', error.message);
 				callback(error);
@@ -135,97 +88,7 @@ HttpAdvancedAccessory.prototype = {
 			}
 		}.bind(this));
 	},
-  
-
-	getLockCurrentState: function(callback){
-		this.log("getLockCurrentState");
-		callback(null, 1); //Not possible with my setup
-	},
-	setLockCurrentState: function(callback){
-		this.log("setLockCurrentState");
-		callback(null, 1); //Not possible with my setup
-	},
-	getLockTargetState: function(callback){
-		this.log("getLockTargetState");
-		callback(null, 1); //Not possible with my setup
-	},
-
-	setLockTargetState: function(powerOn,callback) {
-		var url;
-		var body;
-
-		if (!this.unlock_url || !this.lock_url) {
-			this.log.warn("Ignoring request; No Door url defined.");
-			callback(new Error("No Door url defined."));
-		    return;
-		}
-
-	    if (powerOn) {
-	      url = this.lock_url;
-	      body = this.lock_body;
-	      this.log("Locking Door");
-	    } else {
-      		url = this.unlock_url;
-            body = this.unlock_body;
-      		this.log("Unlocking Door");
-	    }
-		this.httpRequest(url, body, this.http_method, this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
-			if (error) {
-				this.log('HTTP Door function failed: %s', error.message);
-				callback(error);
-			} else {
-				this.log('HTTP Door function succeeded!');
-				callback();
-			}
-		}.bind(this));
-	},
-
-
-	getBrightness: function(callback) {
-		if (!this.brightnesslvl_url) {
-			this.log.warn("Ignoring request; No brightness level url defined.");
-			callback(new Error("No brightness level url defined."));
-			return;
-		}		
-		var url = this.brightnesslvl_url;
-		this.log("Getting Brightness level");
-
-		this.httpRequest(url, "", "GET", this.username, this.password, this.sendimmediately, function(error, response, responseBody) {
-			if (error) {
-				this.log('HTTP get brightness function failed: %s', error.message);
-				callback(error);
-			} else {			
-				var binaryState = parseInt(responseBody);
-				var level = binaryState;
-				this.log("brightness state is currently %s", binaryState);
-				callback(null, level);
-			}
-		}.bind(this));
-	},
-
-	setBrightness: function(level, callback) {
-	
-		if (!this.brightness_url) {
-			this.log.warn("Ignoring request; No brightness url defined.");
-			callback(new Error("No brightness url defined."));
-			return;
-		}    
-		
-		var url = this.brightness_url.replace("%b", level)
-		
-		this.log("Setting brightness to %s", level);
-		
-		this.httpRequest(url, "", this.http_brightness_method, this.username, this.password, this.sendimmediately, function(error, response, body) {
-			if (error) {
-				this.log('HTTP brightness function failed: %s', error);
-				callback(error);
-			} else {
-				this.log('HTTP brightness function succeeded!');
-				callback();
-			}
-		}.bind(this));
-	},
-
+ 
   identify: function(callback) {
     this.log("Identify requested!");
     callback(); // success
@@ -238,87 +101,17 @@ HttpAdvancedAccessory.prototype = {
     var informationService = new Service.AccessoryInformation();
 
     informationService
-    .setCharacteristic(Characteristic.Manufacturer, "HTTP Manufacturer")
-    .setCharacteristic(Characteristic.Model, "HTTP Model")
+    .setCharacteristic(Characteristic.Manufacturer, "myStrom AG")
+    .setCharacteristic(Characteristic.Model, "myStrom WLAN Energy Control Switch")
     .setCharacteristic(Characteristic.SerialNumber, "HTTP Serial Number");
 
-	switch (this.service) {
-	case "Switch":
-		
-
-		if (this.switchHandling == "yes") {
-			this.switchService = new Service.Switch(this.name);			
-			this.switchService
-
-			.getCharacteristic(Characteristic.On)
-			.on('get', function(callback) {callback(null, that.state)})
-			.on('set', this.setPowerState.bind(this));
-			return [this.switchService];
-			break;
-		} else {
-			var switchService = new Service.Switch(this.name);
-			switchService
-			.getCharacteristic(Characteristic.On)	
-			.on('set', this.setPowerState.bind(this));
-		     	return [switchService];
-		}
-        	break;
-	case "Light":
-		var lightbulbService = new Service.Lightbulb(this.name);
-
-		if (this.switchHandling == "yes") {
-			lightbulbService
-			.getCharacteristic(Characteristic.On)
-			.on('get', this.getStatusState.bind(this))
-			.on('set', this.setPowerState.bind(this));
-		} else {
-			lightbulbService
-			.getCharacteristic(Characteristic.On)	
-			.on('set', this.setPowerState.bind(this));
-		}
-
-		if (this.brightnessHandling == "yes") {
-
-	        	lightbulbService
-        		.addCharacteristic(new Characteristic.Brightness())
-		        .on('get', this.getBrightness.bind(this))
-	        	.on('set', this.setBrightness.bind(this));
-      		}
-
-        	return [informationService, lightbulbService];
-		break;
-	case "Door":
-		var lockService = new Service.LockMechanism(this.name);
- 
-		lockService 
-		.getCharacteristic(Characteristic.LockCurrentState)
-		.on('get', this.getLockCurrentState.bind(this))
-		.on('set', this.setLockCurrentState.bind(this));
-
-		lockService 
-		.getCharacteristic(Characteristic.LockTargetState)
-		.on('get', this.getLockTargetState.bind(this))
-		.on('set', this.setLockTargetState.bind(this));
-
-	     	return [lockService];
-		break;
-	case "Smoke":
-		this.smokeService = new Service.MotionSensor(this.name);
-	        
-		this.smokeService
-		.getCharacteristic(Characteristic.SmokeDetected)
-		.on('get', function(callback) {callback(null, that.state)});
-
-		return [this.smokeService];
-	case "Motion":
-		this.motionService = new Service.MotionSensor(this.name);
-	        
-		this.motionService
-		.getCharacteristic(Characteristic.MotionDetected)
-		.on('get', function(callback) {callback(null, that.state)});
-
-	        return [this.motionService];
-        	break;
-	}
+	this.outletService = new Service.Outlet(this.name);			
+	this.outletServiceswitchService
+	 .getCharacteristic(Characteristic.On)
+	 .on('get', function(callback) {callback(null, that.state)})
+	 .on('set', this.setPowerState.bind(this))
+	 .getCharacteristic(Characteristic.OutletInUse)
+	 .on('get', function(callback) {callback(null, that.inUse)});
+	return [this.outletService];
   }
 };
